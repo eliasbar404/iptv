cd1238ca2e8001c3abfbd631969db4631fd9db06
ilyas <ibaraaouche@gmail.com>
2024-11-08 23:59:33 +0100

    create footer and header files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,10 +23,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header/>
+          {children}
+        <Footer/>
       </body>
     </html>
   );
